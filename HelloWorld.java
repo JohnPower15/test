acc925f1958aadcec596dc95ac1be90e092f2d66
John Power <John.Power2@students.ittralee.ie>
2019-11-27 09:40:45 +0000

    Updated

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//My Name
+/*This program sends a simple "HelloWorld" message to the screen*/
+
+public class HelloWorl {
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
