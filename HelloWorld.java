2ac0758b5e329ffca922836abf911fe047d93051
John Power <John.Power2@students.ittralee.ie>
2019-11-27 09:46:42 +0000

    Updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 //My Name
 /*This program sends a simple "HelloWorld" message to the screen*/
+//misspelt World
 
-public class HelloWorl {
+public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
